b8264e0c0bb7ce879e186770684b14581e4b742c
degr8sid-code <58777694+degr8sid-code@users.noreply.github.com>
2020-04-23 19:12:38 +0500

    Update privatenode.py

--- privatenode.py
+++ privatenode.py
@@ -17,17 +17,17 @@ headers = {
 
 url="http://ec2-54-147-37-156.compute-1.amazonaws.com:14265"
 
-response = requests.get(url)
-print(response.status_code)
-#fo = urllib.parse.urlencode(command)
+#response = requests.get(url)
+#print(response.status_code)
+fo = urllib.parse.urlencode(command)
 #fo = urllib2.urlunparse(command)
-#fo = fo.encode('utf-8')
-#print('encoded')
-#request = urllib.request.Request(url, fo)
-#print('requested')
-#returnData = urllib.request.urlopen(request).read()
-#print('url opened')
+fo = fo.encode('utf-8')
+print('encoded')
+request = urllib.request.Request(url, fo)
+print('requested')
+returnData = urllib.request.urlopen(request).read()
+print('url opened')
 
-#jsonData = json.loads(returnData)
+jsonData = json.loads(returnData)
 
-#print (jsonData)
+print (jsonData)
